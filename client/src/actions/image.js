afebdea846fc5b8a74b9c1fcd14fc085e290dcd8
Igor Tkachev <tkacheviscoder@gmail.com>
2019-08-03 15:28:06 +0300

    Добавил image action

---
+++ client/src/actions/image.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+import { GET_IMAGES, IMAGE_ERROR } from '../constants'
+
+// Get All Images
+export const getImages = () => async dispatch => {
+	try {
+		const res = await axios.get('/api/image')
+
+		dispatch({
+			type: GET_IMAGES,
+			payload: res.data,
+		})
+	} catch (err) {
+		dispatch({
+			type: IMAGE_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status },
+		})
+	}
+}

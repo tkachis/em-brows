7a027bb53171212fb29fbbc5676a0ad0cac5ca77
Igor Tkachev <tkacheviscoder@gmail.com>
2019-08-03 11:48:41 +0300

    Добавил компонент footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
 
 function App() {
 	return (
 		<BrowserRouter>
 			<section className="container">
 				<Header />
+				<Footer />
 			</section>
 		</BrowserRouter>
 	)
---
+++ client/src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import styles from './Footer.module.css'
+
+const Footer = () => {
+	return (
+		<div className={styles.footer}>
+			<p>Emma Tkacheva - Brow Master</p>
+			<p>Copyright &copy; 2019, All Rights Reserved</p>
+		</div>
+	)
+}
+
+export default Footer

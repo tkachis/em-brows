529c8e523553bf99f2c399593e6ab7f92acc43f5
Igor Tkachev <tkacheviscoder@gmail.com>
2019-08-04 21:11:56 +0300

    Изменил стили к футеру

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -4,10 +4,10 @@ import styles from './Footer.module.css'
 
 const Footer = () => {
 	return (
-		<div className={styles.footer}>
+		<footer className={styles.footer}>
 			<p>Emma Tkacheva - Brow Master</p>
 			<p>Copyright &copy; 2019, All Rights Reserved</p>
-		</div>
+		</footer>
 	)
 }
 

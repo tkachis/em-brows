54eae6a473a51ac3e393d3506cce45de87a5e94b
Igor Tkachev <tkacheviscoder@gmail.com>
2019-08-08 23:49:59 +0300

    Модернизировал reducer image  для удаления изображения

--- client/src/reducers/image.js
+++ client/src/reducers/image.js
@@ -1,4 +1,4 @@
-import { GET_IMAGES, IMAGE_ERROR } from '../constants'
+import { GET_IMAGES, DELETE_IMAGE, IMAGE_ERROR } from '../constants'
 
 const initialState = {
 	images: [],
@@ -9,6 +9,11 @@ export default (state = initialState, { type, payload }) => {
 	switch (type) {
 		case GET_IMAGES:
 			return { ...state, images: payload }
+		case DELETE_IMAGE:
+			return {
+				...state,
+				images: state.images.filter(image => image._id !== payload),
+			}
 		case IMAGE_ERROR:
 			return { ...state, error: payload }
 		default:

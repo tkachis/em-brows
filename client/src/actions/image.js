e95540c80a2a69e28aa77ea72a46c5a89eac5ade
Igor Tkachev <tkacheviscoder@gmail.com>
2019-08-08 23:48:12 +0300

    Добавил action удаления изображения

--- client/src/actions/image.js
+++ client/src/actions/image.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-import { GET_IMAGES, IMAGE_ERROR } from '../constants'
+import { GET_IMAGES, DELETE_IMAGE, IMAGE_ERROR } from '../constants'
 
 // Get All Images
 export const getImages = () => async dispatch => {
@@ -10,6 +10,25 @@ export const getImages = () => async dispatch => {
 		dispatch({
 			type: GET_IMAGES,
 			payload: res.data,
+		})
+	} catch (err) {
+		console.log(err)
+
+		dispatch({
+			type: IMAGE_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status },
+		})
+	}
+}
+
+// Delete Image
+export const deleteImage = imageId => async dispatch => {
+	try {
+		await axios.delete(`api/image/${imageId}`)
+
+		dispatch({
+			type: DELETE_IMAGE,
+			payload: imageId,
 		})
 	} catch (err) {
 		console.log(err)
--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,7 @@
 // Images
 export const GET_IMAGES = 'GET_IMAGES'
 export const IMAGE_ERROR = 'IMAGE_ERROR'
+export const DELETE_IMAGE = 'DELETE_IMAGE'
 // Auth
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
 export const LOGIN_FAIL = 'LOGIN_FAIL'

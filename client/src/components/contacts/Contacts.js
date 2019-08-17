3080e7e86759e31b7171e0425b564486f52fab90
Igor Tkachev <tkacheviscoder@gmail.com>
2019-08-17 23:20:20 +0300

    Изменил стили компонента Contacts

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -5,14 +5,17 @@ import styles from './Contacts.module.css'
 
 const Contacts = () => (
 	<div className={`${styles.contacts} ${'container'}`}>
-		<YMaps>
-			<Map
-				defaultState={{ center: [59.944227, 30.35826], zoom: 19 }}
-				className={styles.map}
-			>
-				<Placemark geometry={[59.944227, 30.35826]} />
-			</Map>
-		</YMaps>
+		<div className={styles.map}>
+			<YMaps>
+				<Map
+					defaultState={{ center: [59.944227, 30.35826], zoom: 19 }}
+					height={'100%'}
+					width={'100%'}
+				>
+					<Placemark geometry={[59.944227, 30.35826]} />
+				</Map>
+			</YMaps>
+		</div>
 
 		<div className={styles.info}>
 			<h1>Contacts:</h1>

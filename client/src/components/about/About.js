a72a6c0410edf92e131c7010a6d91cf06522fea6
Igor Tkachev <tkacheviscoder@gmail.com>
2019-08-22 18:29:50 +0300

    Анимировал компонент About

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -1,21 +1,37 @@
 import React from 'react'
+import { animated, useSpring } from 'react-spring'
 
 import styles from './About.module.css'
 
-const About = props => {
+const About = () => {
+	const { o, x } = useSpring({
+		from: { o: 0, x: 100 },
+		to: { o: 1, x: 0 },
+	})
+
 	return (
 		<section className={`${styles.about} container`}>
 			<div>
-				<div className={`${styles.image} ${styles.image1}`} />
-
-				<h1>My Studio</h1>
-				<p>
-					Привет! Меня зовут Эмма и я Brow-мастер. В своей работе я
-					придерживаюсь эстетики натуральных образов - люблю естественность и
-					легкость. Меня зовут Эмма и я Brow-мастер. В своей работе я
-					придерживаюсь эстетики натуральных образов - люблю естественность и
-					легкость.
-				</p>
+				<animated.div
+					className={`${styles.image} ${styles.image1}`}
+					style={{
+						transform: x.interpolate(x => `translate3d(-${x}%, 0, 0)`),
+						opacity: o,
+					}}
+				/>
+				<div>
+					<h1>
+						My
+						<br /> Studio
+					</h1>
+					<p>
+						Привет! Меня зовут Эмма и я Brow-мастер. В своей работе я
+						придерживаюсь эстетики натуральных образов - люблю естественность и
+						легкость. Меня зовут Эмма и я Brow-мастер. В своей работе я
+						придерживаюсь эстетики натуральных образов - люблю естественность и
+						легкость.
+					</p>
+				</div>
 			</div>
 			<div>
 				<div>
@@ -32,7 +48,13 @@ const About = props => {
 					</p>
 				</div>
 
-				<div className={`${styles.image} ${styles.image2}`} />
+				<animated.div
+					className={`${styles.image} ${styles.image2}`}
+					style={{
+						transform: x.interpolate(x => `translate3d(${x}%, 0, 0)`),
+						opacity: o,
+					}}
+				/>
 			</div>
 		</section>
 	)
